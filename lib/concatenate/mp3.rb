9e6908014566757535dd509d58133dea92adbfa5
Chirana <thiago.chirana@planisa.com.br>
2024-07-01 02:07:24 -0300

    feat: add functionswq

--- lib/concatenate/mp3.rb
+++ lib/concatenate/mp3.rb
@@ -1,10 +1,16 @@
 # frozen_string_literal: true
+require "colorize"
 
 require_relative "mp3/version"
 
 module Concatenate
   module Mp3
-    class Error < StandardError; end
-    # Your code goes here...
+    class ConcatenateError < StandardError; end
+
+    def files(*files)
+    end
+
+    def process
+    end
   end
 end
